e51102940114a9f8927686dc799fe95ecbf37739
Nhi Nguyen <minhnhi08061105@gmail.com>
2022-10-23 01:10:29 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [UserService, LoggerService],
+  providers: [UserService],
 })
 export class AppComponent {
   constructor(private userService: UserService) {}
